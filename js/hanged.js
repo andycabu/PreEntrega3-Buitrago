9e93769eac48f5cc3c2e6f476f7ba18c1775138d
andycabu-portatil <andycabu@gmail.com>
2023-08-30 15:35:47 +0200

    nuevos estilos tailwindcss y nueva logicas

--- js/hanged.js
+++ js/hanged.js
@@ -1,58 +1,80 @@
-const words = ['javascript', 'programacion', 'html', 'css', 'desarrollo'];
+const words = ["javascript", "programacion", "html", "css", "desarrollo"];
 let currentWord = words[Math.floor(Math.random() * words.length)];
 let guessedLetters = [];
 let remainingAttempts = 6;
 
-const wordDisplay = document.getElementById('word-display');
-const guessesDisplay = document.getElementById('guesses');
-const letterInput = document.getElementById('letter-input');
-const guessButton = document.getElementById('guess-button');
+const wordDisplay = document.getElementById("word-display");
+const guessesDisplay = document.getElementById("guesses");
+const letterInput = document.getElementById("letter-input");
+const guessButton = document.getElementById("guess-button");
 
 function displayWord() {
-    wordDisplay.textContent = currentWord
-        .split('')
-        .map(letter => (guessedLetters.includes(letter) ? letter : '_'))
-        .join(' ');
+  wordDisplay.textContent = currentWord
+    .split("")
+    .map((letter) => (guessedLetters.includes(letter) ? letter : "_"))
+    .join(" ");
 }
 
 function updateGuesses() {
-    guessesDisplay.textContent = `Tus intentos: ${guessedLetters.join(', ')}`;
+  guessesDisplay.textContent = `Tus intentos: ${guessedLetters.join(", ")}`;
+}
+function buttonDisabled() {
+  guessButton.setAttribute("disabled", "true");
+  guessButton.classList.remove("bg-emerald-400");
+  guessButton.classList.add("bg-emerald-200");
+}
+function checkInput() {
+  if (letterInput.value.length === 1) {
+    guessButton.removeAttribute("disabled");
+    guessButton.classList.remove("bg-emerald-200");
+    guessButton.classList.add("bg-emerald-400");
+  } else {
+    buttonDisabled();
+  }
 }
 
 function checkGuess() {
-    const letter = letterInput.value.toLowerCase();
-    if (!guessedLetters.includes(letter)) {
-        guessedLetters.push(letter);
-        if (!currentWord.includes(letter)) {
-            remainingAttempts--;
-        }
-        displayWord();
-        updateGuesses();
-        letterInput.value = '';
-        checkGameStatus();
+  const letter = letterInput.value.toLowerCase();
+  if (!guessedLetters.includes(letter)) {
+    guessedLetters.push(letter);
+    if (!currentWord.includes(letter)) {
+      remainingAttempts--;
+      guessButton.addEventListener("click", buttonDisabled());
     }
+    displayWord();
+    updateGuesses();
+    letterInput.value = "";
+    checkGameStatus();
+  }
 }
 
 function checkGameStatus() {
-    if (currentWord.split('').every(letter => guessedLetters.includes(letter) || remainingAttempts === 0)) {
-        if (remainingAttempts > 0) {
-            alert('¡Ganaste!');
-        } else {
-            alert('Perdiste. La palabra era: ' + currentWord);
-        }
-        resetGame();
+  if (
+    currentWord
+      .split("")
+      .every(
+        (letter) => guessedLetters.includes(letter) || remainingAttempts === 0
+      )
+  ) {
+    if (remainingAttempts > 0) {
+      alert("¡Ganaste!");
+    } else {
+      alert("Perdiste. La palabra era: " + currentWord);
     }
+    resetGame();
+  }
 }
 
 function resetGame() {
-    currentWord = words[Math.floor(Math.random() * words.length)];
-    guessedLetters = [];
-    remainingAttempts = 6;
-    displayWord();
-    updateGuesses();
+  currentWord = words[Math.floor(Math.random() * words.length)];
+  guessedLetters = [];
+  remainingAttempts = 6;
+  displayWord();
+  buttonDisabled();
+  updateGuesses();
 }
 
-guessButton.addEventListener('click', checkGuess);
+guessButton.addEventListener("click", checkGuess);
 
 displayWord();
 updateGuesses();
--- js/memorie.js
+++ js/memorie.js
@@ -1,100 +1,102 @@
 const cards = [
-    '/assets/images/pirata-con-bandera.png',
-    '/assets/images/pirata-con-bandera.png',
-    '/assets/images/cofre.png',
-    '/assets/images/cofre.png',
-    '/assets/images/mapa-tesoro.png',
-    '/assets/images/mapa-tesoro.png',
-    '/assets/images/mujer-pirata.png',
-    '/assets/images/mujer-pirata.png',
-    '/assets/images/mujer-pirata-con-bandera.png',
-    '/assets/images/mujer-pirata-con-bandera.png',
-    '/assets/images/pirata-con-espada.png',
-    '/assets/images/pirata-con-espada.png',
-    '/assets/images/pirata-con-espada-madera.png',
-    '/assets/images/pirata-con-espada-madera.png',
-    '/assets/images/pirata-con-loro.png',
-    '/assets/images/pirata-con-loro.png',
-    '/assets/images/pirata-con-mono.png',
-    '/assets/images/pirata-con-mono.png',
-    '/assets/images/pirata-con-perro.png',
-    '/assets/images/pirata-con-perro.png',
+  "/assets/images/pirata-con-bandera.png",
+  "/assets/images/pirata-con-bandera.png",
+  "/assets/images/cofre.png",
+  "/assets/images/cofre.png",
+  "/assets/images/mapa-tesoro.png",
+  "/assets/images/mapa-tesoro.png",
+  "/assets/images/mujer-pirata.png",
+  "/assets/images/mujer-pirata.png",
+  "/assets/images/mujer-pirata-con-bandera.png",
+  "/assets/images/mujer-pirata-con-bandera.png",
+  "/assets/images/pirata-con-espada.png",
+  "/assets/images/pirata-con-espada.png",
+  "/assets/images/pirata-con-espada-madera.png",
+  "/assets/images/pirata-con-espada-madera.png",
+  "/assets/images/pirata-con-loro.png",
+  "/assets/images/pirata-con-loro.png",
+  "/assets/images/pirata-con-mono.png",
+  "/assets/images/pirata-con-mono.png",
+  "/assets/images/pirata-con-perro.png",
+  "/assets/images/pirata-con-perro.png",
 ];
 
-const memoryBoard = document.querySelector('.memory-board');
+const memoryBoard = document.querySelector(".memory-board");
 let flippedCards = [];
 let matchedCards = [];
 
 function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
+  return array.sort(() => Math.random() - 0.5);
 }
 
 function createCard(imagePath) {
-    const card = document.createElement('div');
-    card.className = 'card';
+  const card = document.createElement("div");
+  card.className =
+    "card relative w-40 h-52 bg-emerald-400 flex cursor-pointer border border-solid border-emerald-400  duration-500";
 
-    const front = document.createElement('div');
-    front.className = 'front';
+  const front = document.createElement("div");
+  front.className = "front";
 
-    const back = document.createElement('div');
-    back.className = 'back';
+  const back = document.createElement("div");
+  back.className = "back";
 
-    const img = document.createElement('img');
-    img.src = imagePath;
-    back.appendChild(img);
+  const img = document.createElement("img");
+  img.src = imagePath;
+  back.appendChild(img);
 
-    card.appendChild(back);
-    card.appendChild(front);
+  card.appendChild(back);
+  card.appendChild(front);
 
-    card.addEventListener('click', () => flipCard(card));
-    return card;
+  card.addEventListener("click", () => flipCard(card));
+  return card;
 }
 
 function flipCard(card) {
-    if (!flippedCards.includes(card) && flippedCards.length < 2 && !matchedCards.includes(card)) {
-        card.classList.add('flipped');
-        flippedCards.push(card);
+  if (
+    !flippedCards.includes(card) &&
+    flippedCards.length < 2 &&
+    !matchedCards.includes(card)
+  ) {
+    card.classList.add("flipped");
+    flippedCards.push(card);
 
-        if (flippedCards.length === 2) {
-            setTimeout(checkForMatch, 1000);
-        }
+    if (flippedCards.length === 2) {
+      setTimeout(checkForMatch, 1000);
     }
+  }
 }
 
 function checkForMatch() {
-    const [card1, card2] = flippedCards;
-    const image1 = card1.querySelector('.back img').src;
-    const image2 = card2.querySelector('.back img').src;
+  const [card1, card2] = flippedCards;
+  const image1 = card1.querySelector(".back img").src;
+  const image2 = card2.querySelector(".back img").src;
 
-    if (image1 === image2) {
-        matchedCards.push(card1, card2);
-        flippedCards = [];
+  if (image1 === image2) {
+    matchedCards.push(card1, card2);
+    flippedCards = [];
 
-        if (matchedCards.length === cards.length) {
-            alert('¡Has ganado!');
-            resetGame();
-        }
-    } else {
-        setTimeout(() => {
-            flippedCards.forEach(card => card.classList.remove('flipped'));
-            flippedCards = [];
-        }, 1000);
+    if (matchedCards.length === cards.length) {
+      alert("¡Has ganado!");
+      resetGame();
     }
+  } else {
+    setTimeout(() => {
+      flippedCards.forEach((card) => card.classList.remove("flipped"));
+      flippedCards = [];
+    }, 1000);
+  }
 }
 
 function resetGame() {
-    memoryBoard.innerHTML = ''; 
-    initializeGame(cards);
+  memoryBoard.innerHTML = "";
+  initializeGame(cards);
 }
 
 function initializeGame(cards) {
-    shuffle(cards);
-    cards.forEach(imagePath => {
-        const card = createCard(imagePath);
-        memoryBoard.appendChild(card);
-    });
+  shuffle(cards);
+  cards.forEach((imagePath) => {
+    const card = createCard(imagePath);
+    memoryBoard.appendChild(card);
+  });
 }
-initializeGame(cards)
-
-
-
+initializeGame(cards);
